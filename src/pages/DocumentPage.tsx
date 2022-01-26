33b1b741eba03c2e803a45bcd1ca9fcff0dfdd0e
SnO2WMaN <me@sno2wman.net>
2022-01-27 06:44:23 +0900

    send commit

--- src/pages/DocumentPage.tsx
+++ src/pages/DocumentPage.tsx
@@ -1,27 +1,89 @@
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import useSWR from "swr";
 import { Document, Lines } from "~/components/Document";
 import { CaptureData } from "~/components/types";
-import { createLineId } from "~/generators/id";
+import { createCommitId } from "~/generators/id";
 
-export const calcDocumentEndpoint = (id: string) =>
-  "http://0.0.0.0:8000/docs/" + id;
+export const calcDocumentEditEndpoint = (id: string) =>
+  "ws://0.0.0.0:8000/docs/" + id + "/edit";
+
+export type Document = {
+  lines: Lines;
+};
+
+export const useEdit = (
+  endpoint: string | undefined
+):
+  | {
+      ready: false;
+      document: undefined;
+    }
+  | {
+      ready: true;
+      document: Document;
+      sendCommit: (payload: CaptureData) => void;
+    } => {
+  const wsRef = useRef<WebSocket>();
+
+  const [document, setDocuments] = useState<Document>();
+  const [previousCommit, setPreviousCommit] = useState<{ commmitId: string }>();
+
+  useEffect(() => {
+    if (!endpoint) return;
+
+    wsRef.current = new WebSocket(endpoint);
+    wsRef.current.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      if (data.method === "INIT") {
+        const payload = data.payload;
+        setDocuments(payload.document);
+        setPreviousCommit(payload.latestCommit);
+      }
+    });
+  }, [endpoint]);
+
+  const sendCommit = (payload: CaptureData) => {
+    if (!wsRef.current) return;
+    if (!previousCommit) return;
+
+    const previousCommitId = previousCommit.commmitId;
+    const newCommitId = createCommitId();
+
+    wsRef.current.send(
+      JSON.stringify({
+        previousCommitId: previousCommitId,
+        newCommitId,
+        payload,
+      })
+    );
+    setPreviousCommit(() => ({ commmitId: newCommitId }));
+  };
+
+  if (!!wsRef.current && !!document && !!previousCommit) {
+    return {
+      ready: true,
+      document,
+      sendCommit: sendCommit,
+    };
+  } else {
+    return {
+      ready: false,
+      document: undefined,
+    };
+  }
+};
 
 export const DocumentPage: React.VFC = () => {
   const { id } = useParams<"id">();
-  const { data } = useSWR<{ lines: Lines }>(id && calcDocumentEndpoint(id));
-
-  const handleMethods = (data: CaptureData) => {
-    console.dir(data);
-  };
+  const edit = useEdit(id ? calcDocumentEditEndpoint(id) : undefined);
 
   return (
     <div style={{ margin: "64px 64px" }}>
       <span>{id}</span>
-      {data && (
+      {edit.ready && (
         <Document
-          initLines={data.lines}
-          handleMethod={handleMethods}
+          initLines={edit.document.lines}
+          handleMethod={edit.sendCommit}
         ></Document>
       )}
     </div>
